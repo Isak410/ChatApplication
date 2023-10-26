065409402f6853f7139c259ade7fb3985fa5a943
Isak410 <Isakstenep@gmail.com>
2023-10-26 12:21:27 +0200

    awfawfawbfawfbaw

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
-console.log("Hei Hei")
+const knapp = document.getElementById('knapp')
+const inputBruker = document.getElementById('input0')
+const inputPassord = document.getElementById('input1')
+
+
+function loginTest() {
+    for (let i = 0; i < 2; i++){if (document.getElementById('input'+i).value == "") {return}}
+
+    
+    console.log(inputBruker.value)
+    console.log(inputPassord.value)
+}
+
+
+
+knapp.addEventListener('click', loginTest)
--- server.js
+++ server.js
@@ -5,9 +5,18 @@ const server = http.createServer(app);
 const session = require('express-session')
 //const router = express.Router()
 
+var users = {
+    'id1':{username:'Isak', password:'Isak123'},
+    'id2':{username:'Adrian', password:'Adrian123'}
+}
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
+
+app.post('/login', (req,res) => {
+    res
+})
 
 app.use(express.json())
 app.use(express.static('public'));
