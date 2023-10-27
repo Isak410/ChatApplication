9cc6c0f24c93dedc07334f2bbdab5f508eea4309
Isak410 <113894543+Isak410@users.noreply.github.com>
2023-10-28 01:50:18 +0200

    Innlogging funker
    neste er opprett bruker

--- public/script.js
+++ public/script.js
@@ -1,22 +1,33 @@
+document.addEventListener('DOMContentLoaded',  () => {
+
 const knapp = document.getElementById('knapp')
 const inputBruker = document.getElementById('input0')
 const inputPassord = document.getElementById('input1')
 const knapp1 = document.getElementById('brukerKnapp')
+const logindiv = document.getElementById('logindiv')
+const formdiv = document.getElementById('formDiv')
+const loginField = document.getElementById('loginField')
+
+var socket = io();
 
 async function getMyUsername() {
-    console.log("afkjlalkwjla")
     const username = await fetch('/sendUsername')
     .then(res => res.json())
     console.log(username)
+}
+
+function displayChat() {
+    logindiv.style.display = "none"
+    formdiv.style.display = "block"
     
 }
 
-async function loginTest() {
+function loginTest() {
     for (let i = 0; i < 2; i++){if (document.getElementById('input'+i).value == "") {return}}
     console.log(inputBruker.value)
     let username = inputBruker.value
     let password = inputPassord.value
-    var response = await fetch('/login', {
+    fetch('/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -24,9 +35,21 @@ async function loginTest() {
         body: JSON.stringify({ username, password }),
     })
     .then (res => res.json())
-    console.log(response)
+    .then (data => {
+        console.log(data.success)
+        if (data.success == false) {loginField.innerHTML = "wrong username or password"; return}
+            displayChat()
+    })
 }
 
+fetch('/checkSession')
+.then(res => res.json())
+.then(data => {
+    if (data == true) {
+        displayChat()
+        console.log("data true")
+    }   else {console.log("data false")}
+})
 
-knapp1.addEventListener('click', getMyUsername)
 knapp.addEventListener('click', loginTest)
+})
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const session = require('express-session')
-const sessionLength = 1000*60*10
+const sessionLength = 1*60*5 //hvor mange sekunder til session slettes
 //const router = express.Router()
-var sessions
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 var users = {
     'id1':{username:'Isak', password:'Isak123'},
     'id2':{username:'Adrian', password:'Adrian123'}
 }
+
+
 
 app.use(express.json())
 
@@ -22,47 +25,54 @@ app.use(session({
     secret: 'secret-key',
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge:sessionLength }
+    cookie: { maxAge:sessionLength*1000 }
 }))
 
 function cookieStillValid(cookie) {
-    if (cookie._expires < new Date()) {
+    console.log(new Date())
+    if (cookie.expires < new Date()) {
         return false
     } else {return true}
 }
 
 app.get('/sendUsername', (req,res) => {
-    //sessions = req.session
-    if (cookieStillValid(req.session.cookie) == false) {
-        req.session.destroy()
-        console.log("session destroyed")
-        return
+    if (!req.session.username || !cookieStillValid(req.session.cookie)) {
+        req.session.destroy();
+        console.log("session destroyed");
+        res.json('No active session');
+        return;
     }
-    console.log(sessions.username)
-    res.json(sessions.username)
-    
+    console.log(req.session.username);
+    res.json(req.session.username);
 })
 
-app.post('/login', (req,res) => {
-    if (cookieStillValid(req.session.cookie) == false) {
-        req.session.destroy()
-        console.log("session destroyed")
-        return
-    }
-    const { username, password} = req.body
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
     const userList = Object.values(users);
     const user = userList.find(u => u.username === username && u.password === password);
+    console.log(user)
     if (user) {
-        res.json('Login successful!');
-        
-        sessions = req.session
-        sessions.username = username
-        console.log(req.session)
-      } else {
-        res.json('Invalid username or password');
-      }
+        req.session.username = username;
+        res.json({success:true});
+    } else {
+        res.json({success:false})
+    }
+});
+
+app.get('/checkSession', (req,res) => {
+    if (req.session.username) {
+        var bool = cookieStillValid(req.session.cookie)
+        if (bool == true) {
+            res.json(true)
+        }   else {req.session.destroy();res.json(false)}
+    }   else {res.json(false)}             
 })
 
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
 
 app.use(express.static('public'));
 
