cd9cf28bda0a81cf76a4cc37448642f52b055ce6
Isak410 <Isakstenep@gmail.com>
2023-10-27 11:41:57 +0200

    Sessions

--- public/script.js
+++ public/script.js
@@ -1,16 +1,32 @@
 const knapp = document.getElementById('knapp')
 const inputBruker = document.getElementById('input0')
 const inputPassord = document.getElementById('input1')
+const knapp1 = document.getElementById('brukerKnapp')
 
-
-function loginTest() {
-    for (let i = 0; i < 2; i++){if (document.getElementById('input'+i).value == "") {return}}
-
+async function getMyUsername() {
+    console.log("afkjlalkwjla")
+    const username = await fetch('/sendUsername')
+    .then(res => res.json())
+    console.log(username)
     
+}
+
+async function loginTest() {
+    for (let i = 0; i < 2; i++){if (document.getElementById('input'+i).value == "") {return}}
     console.log(inputBruker.value)
-    console.log(inputPassord.value)
+    let username = inputBruker.value
+    let password = inputPassord.value
+    var response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then (res => res.json())
+    console.log(response)
 }
 
 
-
+knapp1.addEventListener('click', getMyUsername)
 knapp.addEventListener('click', loginTest)
--- server.js
+++ server.js
@@ -3,29 +3,68 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const session = require('express-session')
+const sessionLength = 1000*60*10
 //const router = express.Router()
+var sessions
 
 var users = {
     'id1':{username:'Isak', password:'Isak123'},
     'id2':{username:'Adrian', password:'Adrian123'}
 }
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/login', (req,res) => {
-    res
-})
-
-app.use(express.json())
-app.use(express.static('public'));
 app.use(session({
     secret: 'secret-key',
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge:sessionLength }
 }))
 
+function cookieStillValid(cookie) {
+    if (cookie._expires < new Date()) {
+        return false
+    } else {return true}
+}
+
+app.get('/sendUsername', (req,res) => {
+    //sessions = req.session
+    if (cookieStillValid(req.session.cookie) == false) {
+        req.session.destroy()
+        console.log("session destroyed")
+        return
+    }
+    console.log(sessions.username)
+    res.json(sessions.username)
+    
+})
+
+app.post('/login', (req,res) => {
+    if (cookieStillValid(req.session.cookie) == false) {
+        req.session.destroy()
+        console.log("session destroyed")
+        return
+    }
+    const { username, password} = req.body
+    const userList = Object.values(users);
+    const user = userList.find(u => u.username === username && u.password === password);
+    if (user) {
+        res.json('Login successful!');
+        
+        sessions = req.session
+        sessions.username = username
+        console.log(req.session)
+      } else {
+        res.json('Invalid username or password');
+      }
+})
+
+
+app.use(express.static('public'));
 
 server.listen(8080, () => {
     console.log('Listening on 8080');
